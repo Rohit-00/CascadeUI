2b6ce88f260aae5d0e35dcfc516e6f490a2dcf13
Rohit <rr1796@dseu.ac.in>
2024-10-14 18:15:46 +0530

    added components

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -1,29 +1,28 @@
-import { Iphone15Pro } from "./iphone"
-
-const comps = [
-    {src:'https://i.ibb.co/gdRH7wT/Animated-Buttongif.webp',title:''},
-    {src:'https://i.ibb.co/bHnBKd3/on-Board-Animation.webp',title:''},
-    {src:'https://i.ibb.co/SQXT9gL/Multi-Step-Loader-ezgif-com-video-to-webp-converter.webp',title:''},
-    {src:'https://i.ibb.co/GsSFXyz/Animated-Listt-ezgif-com-video-to-webp-converter-1.webp',title:''},
-    {src:'https://i.ibb.co/nRyDfw3/Credit-Card-ezgif-com-video-to-webp-converter.webp',title:''},
-    {src:'',title:''},
+    const comps = [
+    {src:'https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/670bf4250008ecb6c9a5/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin',title:''},
+    {src:'https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/670d0a40001a1506aea3/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin',title:''},
+    {src:'https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/670d0a3500172ecc3b46/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin',title:''},
+    {src:'https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/670d0a2c001ca6043c15/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin',title:''},
+    {src:'https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/670bfa57002fc8873ab8/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin',title:''},
 ]
 
 const Screen = ({item}:any) =>{
     return(
-        <Iphone15Pro src={item.src} className="  h-[400px] w-[200px]  m-10 transition-transform ease-in-out hover:scale-110 cursor-pointer rounded-lg"></Iphone15Pro>
+      <div className="mt-9 h-24 "> <video height={400} width={200} src={item.src} loop autoPlay muted  className="rounded-xl transition-transform ease-in-out hover:scale-110 cursor-pointer" /> </div> 
     )
 }
 
 function Components() {
   return (
     <div className="mt-9">
-      <div className="text-3xl sm:text-6xl font-semibold">Components</div>
+      <div className="flex flex-row justify-between items-baseline"><div className="text-3xl sm:text-6xl font-semibold">Components</div><div>all components</div></div>
       <div className="grid sm:grid-cols-5 justify-center items-center ">
-    {comps.map((item)=>
-    <div><Screen item={item}/></div>
-    )}
-      </div>
+      {comps.map((item)=>
+        <div>
+            <Screen item={item} />
+        </div>
+    )} 
+          </div>
     </div>
   )
 }
