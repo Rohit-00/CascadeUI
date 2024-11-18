6ef32bad6fcb83af72cf49ae8988dd1cf7f5cecc
Rohit <rr1796@dseu.ac.in>
2024-11-18 16:40:27 +0530

    improved landing page

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -4,8 +4,8 @@ function Code() {
   return (
     <div className="mt-16 ">
       <div className="sm:text-5xl font-bold text-3xl text-center mb-11">Reusable Components</div>
-      <div className="mt-5 flex sm:flex-row flex-col justify-evenly items-center gap-4">
-      <div className="h-[25rem] sm:w-[32rem] bg-slate-800 px-5">
+      <div className="mt-5 flex sm:flex-row flex-col justify-evenly items-center gap-4 mx-5">
+      <div className="sm:h-[25rem] h-[19rem] w-[21.5rem] sm:w-[32rem] bg-slate-800 px-5">
         <div className="flex flex-row gap-2 pt-4">
             <div className="h-5 w-5 rounded-full bg-red-500"/>
             <div className="h-5 w-5 rounded-full bg-yellow-500"/>
@@ -32,7 +32,7 @@ function Code() {
           </pre>
         </div>
       </div>
-      <Iphone15Pro className="size-[30rem] max-w-fit" src="https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/6729051300136c77b52c/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin"/>
+      <Iphone15Pro className="size-[30rem] max-w-fit self-center" src="https://cloud.appwrite.io/v1/storage/buckets/670bf3d800356f2d1307/files/6729051300136c77b52c/view?project=66cc3f88001fcc4896d5&project=66cc3f88001fcc4896d5&mode=admin"/>
       </div>
       
     </div>
--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -23,31 +23,33 @@ function MyComponent() {
   };
 
   return (
-    <section className="py-16 mt-11">
-      <div className="container mx-auto px-6">
-        <h2 className="text-3xl sm:text-5xl font-bold text-center">Quick Start Guide</h2>
-        <div className="text-center font-base mb-11">Easy to implement components</div>
-        <div className="max-w-3xl mx-auto">
-          <div className="flex mb-4">
-            <button
-              className={`py-2 px-4 font-semibold ${activeTab === 'installation' ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-600'}`}
-              onClick={() => setActiveTab('installation')}
-            >
-              Installation
-            </button>
-            <button
-              className={`py-2 px-4 font-semibold ${activeTab === 'usage' ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-600'}`}
-              onClick={() => setActiveTab('usage')}
-            >
-              Usage
-            </button>
+    <div className="sm:flex sm:items-center sm:justify-center">
+      <section className="py-16 rounded-lg shadow-lg">
+        <div className="container">
+          <h2 className="text-3xl sm:text-5xl font-bold text-center">Quick Start Guide</h2>
+          <div className="font-base mb-11 text-center">Easy to implement components</div>
+          <div className="max-w-3xl mx-auto">
+            <div className="flex sm:justify-center">
+              <button
+                className={`py-2 px-4 font-semibold ${activeTab === 'installation' ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-600'}`}
+                onClick={() => setActiveTab('installation')}
+              >
+                Installation
+              </button>
+              <button
+                className={`py-2 px-4 font-semibold ${activeTab === 'usage' ? 'text-blue-600 border-b-2 border-blue-600' : 'text-gray-600'}`}
+                onClick={() => setActiveTab('usage')}
+              >
+                Usage
+              </button>
+            </div>
+            <pre className="bg-gray-800 text-white p-4 rounded-lg overflow-x-auto">
+              <code>{quickStartTabs[activeTab]}</code>
+            </pre>
           </div>
-          <pre className="bg-gray-800 text-white p-4 rounded-lg overflow-x-auto">
-            <code>{quickStartTabs[activeTab]}</code>
-          </pre>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
 
